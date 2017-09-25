5f1283c96b8c2630af07262db25a1906eca2aeaa
ljnissen <lj_nissen@gmx.de>
2017-09-25 09:15:52 +0000

    Fix simple login & flash

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   
 
+  get 'sessions/new'
+
   root 'demo#index'
   get '/about', to: 'demo#about'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   
 
