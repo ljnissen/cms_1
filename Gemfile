18305abbf7bf72917c26772335a620be0a57a3bc
ljnissen <lj_nissen@gmx.de>
2017-09-26 18:54:54 +0000

    Update & delete

--- Gemfile
+++ Gemfile
@@ -4,7 +4,14 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
 
+
+
 gem 'bcrypt', '3.1.11'
+
+gem 'will_paginate',           '3.1.5'
+
+gem 'bootstrap-will_paginate', '1.0.0'
+
 
 gem 'bootstrap-sass', '3.3.7'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "Kalle Anka",
+			email: "kanka@ankeborg.se",
+			password: "plm123!!",
+			password_confirmation: "plm123!!",
+			admin: true)
