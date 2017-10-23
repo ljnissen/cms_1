be8117954cbed0ae921dce229f548cd3358e7949
ljnissen <lj_nissen@gmx.de>
2017-10-23 17:25:10 +0000

    Fix sections controller

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,9 +2,10 @@ class SectionsController < ApplicationController
   before_action :logged_in_user
   # before_action :correct_user
   before_action :admin_user
+  before_action :find_page
 
   def index
-    @sections = Section.order("position ASC")
+    @sections = Section.where(:page_id => @page.id).order('position ASC')
   end
 
   def show
@@ -12,15 +13,17 @@ class SectionsController < ApplicationController
   end
 
   def new
-    @section = Section.new
+    @section = Section.new({:page_id => @page.id}).order('position ASC')
+    @pages = Page.order('position ASC')
   end
 
   def create
     @section = Section.new(section_params)
     if @section.save
       flash[:success] = "New section created."
-      redirect_to(:action => "index")
+      redirect_to(:action => "index", :page_id => @page.id)
     else
+      @pages = Page.order('position ASC')
       render("new")
     end
   end
@@ -33,7 +36,7 @@ class SectionsController < ApplicationController
     @section = Section.find(params[:id])
     if @section.update_attributes(section_params)
       flash[:success] = "Section updated successfully."
-      redirect_to(:action => 'show', :id => @section.id)
+      redirect_to(:action => 'show', :id => @section.id, :page_id => @page.id)
     else
       render('edit')
     end
@@ -47,7 +50,7 @@ class SectionsController < ApplicationController
     @section = Section.find(params[:id])
     @section.destroy
     flash[:success] = "Section destroyed successfully."
-    redirect_to(:action => 'index')
+    redirect_to(:action => 'index', :page_id => @page.id)
   end
 
   private
@@ -72,5 +75,11 @@ class SectionsController < ApplicationController
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+
+    def find_page
+      if params[:page_id]
+        @page = Page.find(params[:page_id])
+      end
     end
 end
